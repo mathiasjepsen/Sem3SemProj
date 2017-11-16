755f477d939884bca9529af25196cd06839d01ec
mathiasjepsen <mathias@wpsnet.com>
2017-11-16 08:35:18 +0100

    Testing Travis

--- seedServer/src/main/java/deploy/DeploymentConfiguration.java
+++ seedServer/src/main/java/deploy/DeploymentConfiguration.java
@@ -1,11 +1,12 @@
 package deploy;
 
+import entity.Address;
+import entity.Place;
 import entity.Role;
 import entity.User;
-import facades.UserFacade;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Map;
+import java.util.HashMap;
 import java.util.Properties;
 
 import java.util.logging.Level;
@@ -16,6 +17,7 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import security.PasswordStorage;
 import security.Secret;
 
 @WebListener
@@ -48,37 +50,65 @@ public class DeploymentConfiguration implements ServletContextListener {
     }
     ServletContext context = sce.getServletContext();
  
-    boolean makeTestUser = context.getInitParameter("makeTestUser").toLowerCase().equals("true");
-    if (makeTestUser) {
-      EntityManager em = Persistence.createEntityManagerFactory("pu_development").createEntityManager();
-      try {
-        System.out.println("Creating TEST Users");
-        if (em.find(User.class, "user") == null) {
-          em.getTransaction().begin();
-          Role userRole = new Role("User");
-          Role adminRole = new Role("Admin");
-          User user = new User("user", "test");
-          user.addRole(userRole);
-          User admin = new User("admin", "test");
-          admin.addRole(adminRole);
-          User both = new User("user_admin", "test");
-          both.addRole(userRole);
-          both.addRole(adminRole);
-          em.persist(userRole);
-          em.persist(adminRole);
-          em.persist(user);
-          em.persist(admin);
-          em.persist(both);
-          em.getTransaction().commit();
-          System.out.println("Created TEST Users");
-        }
-      } catch (Exception ex) {
-        Logger.getLogger(UserFacade.class.getName()).log(Level.SEVERE, null, ex);
-        em.getTransaction().rollback();
-      } finally {
-        em.close();
-      }
-    }
+//        EntityManager em = Persistence.createEntityManagerFactory("pu_development").createEntityManager();
+//        try {
+//            em.getTransaction().begin();
+//            Address address = new Address("London", "2000", "First Avenue", "124312");
+//            Address address2 = new Address("Paris", "3000", "Second Boulevard", "42142");
+//            Address address3 = new Address("Copenhagen", "4000", "Third Quadrant", "12412");
+//            User user1 = new User("mathias", "1234", "Mathias", "Jepsen", "123325234", "mathias@wpsnet.com");
+//            User user2 = new User("thomas", "1234", "Thomas", "Thimothee", "23442635", "thomas@thom.com");
+//            User user3 = new User("dimitri", "1234", "Dim", "Dimitri", "456789", "dim@dim.com");
+//            User user4 = new User("mikael", "1234", "Mik", "Mak", "42421", "mik@mik.com");
+//            User user5 = new User("martin", "1234", "Martin", "Lannister", "435323", "martin@mail.com");
+//            User user6 = new User("lovro", "1234", "Lovro", "Louvrou", "424", "louv@dim.com");
+//            User admin = new User("admin", "1234");
+//            Role role = new Role("User");
+//            Role adminRole = new Role("Admin");
+//            user1.addRole(role);
+//            user2.addRole(role);
+//            user3.addRole(role);
+//            user4.addRole(role);
+//            user5.addRole(role);
+//            user6.addRole(role);
+//            admin.addRole(adminRole);
+//            HashMap<String, Double> rating = new HashMap();
+//            rating.put(user1.getUserName(), 2.0);
+//            rating.put(user2.getUserName(), 3.0);
+//            rating.put(user3.getUserName(), 4.0);
+//            String image = "pocketFitIcon.png";
+//            Place place = new Place(address, "A stroll down memory lane", rating, image);
+//            HashMap<String, Double> rating2 = new HashMap();
+//            rating2.put(user1.getUserName(), 5.0);
+//            rating2.put(user2.getUserName(), 4.0);
+//            rating2.put(user3.getUserName(), 4.0);
+//            Place place2 = new Place(address2, "A breath of fresh air", rating2, image);
+//            HashMap<String, Double> rating3 = new HashMap();
+//            rating3.put(user1.getUserName(), 1.0);
+//            rating3.put(user2.getUserName(), 2.0);
+//            rating3.put(user3.getUserName(), 3.0);
+//            Place place3 = new Place(address3, "Once in a lifetime experience", rating3, image);
+//            em.persist(address);
+//            em.persist(address2);
+//            em.persist(address3);
+//            em.persist(place);
+//            em.persist(place2);
+//            em.persist(place3);
+//            em.persist(user1);
+//            em.persist(user2);
+//            em.persist(user3);
+//            em.persist(user4);
+//            em.persist(user5);
+//            em.persist(user6);
+//            em.persist(admin);
+//            em.persist(role);
+//            em.persist(adminRole);
+//            em.getTransaction().commit();
+//        } catch (PasswordStorage.CannotPerformOperationException ex) {
+//            Logger.getLogger(DeploymentConfiguration.class.getName()).log(Level.SEVERE, null, ex);
+//        } finally {
+//            em.close();
+//        }
   }
 
   @Override
--- seedServer/src/test/java/test/utils/EmbeddedTomcat.java
+++ seedServer/src/test/java/test/utils/EmbeddedTomcat.java
@@ -1,7 +1,5 @@
 package test.utils;
 
-
-
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -18,59 +16,58 @@ import org.glassfish.jersey.servlet.ServletContainer;
 
 public class EmbeddedTomcat {
 
-  private final static String TMP_DIR = System.getProperty("java.io.tmpdir")+"/EmbeddedTomcat";
-  //private static final int PORT = 9999;
-  
-  Tomcat tomcat;
+    private final static String TMP_DIR = System.getProperty("java.io.tmpdir") + "/EmbeddedTomcat";
+    //private static final int PORT = 9999;
 
+    Tomcat tomcat;
 
-  public void start(int port,String appContext) throws ServletException,
-          MalformedURLException,
-          LifecycleException {
-    // Define a folder to hold web application contents.Not used by app, but is required by Tomcat. This directory MUST exist
-    String webAppLocation = "WebContent/";
-    tomcat = new Tomcat();
+    public void start(int port, String appContext) throws ServletException,
+            MalformedURLException,
+            LifecycleException {
+        // Define a folder to hold web application contents.Not used by app, but is required by Tomcat. This directory MUST exist
+        String webAppLocation = "WebContent/";
+        tomcat = new Tomcat();
 
-    new File("WebContent").mkdir();
+        new File("WebContent").mkdir();
 
-    // new java.io.File("tmp").mkdir();
-    tomcat.setBaseDir(TMP_DIR);
-    tomcat.setPort(port);
+        // new java.io.File("tmp").mkdir();
+        tomcat.setBaseDir(TMP_DIR);
+        tomcat.setPort(port);
 
-    // Define a web application context.
-    Context context = tomcat.addWebapp(appContext, new File(webAppLocation).getAbsolutePath());
-    // Add servlet that will register Jersey REST resources
-    Tomcat.addServlet(context, "jersey-container-servlet", resourceConfig());
-    context.addServletMapping("/api/*", "jersey-container-servlet");
-    tomcat.start();
-    Server server = tomcat.getServer();
-    System.out.println("Address: "+server.getAddress());
-    for(Service s : server.findServices()){
-      System.out.println("Service: "+s.getDomain()+" -- "+s.getName());
+        // Define a web application context.
+        Context context = tomcat.addWebapp(appContext, new File(webAppLocation).getAbsolutePath());
+        // Add servlet that will register Jersey REST resources
+        Tomcat.addServlet(context, "jersey-container-servlet", resourceConfig());
+        context.addServletMapping("/api/*", "jersey-container-servlet");
+        tomcat.start();
+        Server server = tomcat.getServer();
+        System.out.println("Address: " + server.getAddress());
+        for (Service s : server.findServices()) {
+            System.out.println("Service: " + s.getDomain() + " -- " + s.getName());
+        }
+        //Don't comment the line below in unless you know what you do. It will block here, waiting for the server to stop. 
+        //This can be usefull for testing the embedded server from a browser or postman
+        //tomcat.getServer().await();
     }
-    //Don't comment the line below in unless you know what you do. It will block here, waiting for the server to stop. 
-    //This can be usefull for testing the embedded server from a browser or postman
-    //tomcat.getServer().await();
-  }
 
-  private ServletContainer resourceConfig() {
-    //Load the Wizard-generated rest.ApplicationConfig - class
-    Set<Class<?>> resources = new rest.ApplicationConfig().getClasses();
-    System.out.println("Loaded Classes Count: "+resources.size());
-    return new ServletContainer(new ResourceConfig(resources));
-    //return new ServletContainer(new ResourceConfig(new rest.ApplicationConfig().getClasses()));
-  }
-
-  public void stop() throws IOException {
-    try {
-      tomcat.stop();
-      tomcat.destroy();
-      //Delete our embedded Tomcat's temp  + WebContent folder
-      FileUtils.deleteDirectory(new File("TMP_DIR"));
-      FileUtils.deleteDirectory(new File("WebContent"));
-    } catch (LifecycleException e) {
-      throw new RuntimeException(e);
-
+    private ServletContainer resourceConfig() {
+        //Load the Wizard-generated rest.ApplicationConfig - class
+        Set<Class<?>> resources = new rest.ApplicationConfig().getClasses();
+        System.out.println("Loaded Classes Count: " + resources.size());
+        return new ServletContainer(new ResourceConfig(resources));
+        //return new ServletContainer(new ResourceConfig(new rest.ApplicationConfig().getClasses()));
     }
-  }
+
+    public void stop() throws IOException {
+        try {
+            tomcat.stop();
+            tomcat.destroy();
+            //Delete our embedded Tomcat's temp  + WebContent folder
+            FileUtils.deleteDirectory(new File("TMP_DIR"));
+            FileUtils.deleteDirectory(new File("WebContent"));
+        } catch (LifecycleException e) {
+            throw new RuntimeException(e);
+
+        }
+    }
 }
