18ea3d0cfded168b77b086d060e6ab38e1a20134
dimitri <cph-dt46@cphbusiness.dk>
2017-11-28 17:22:05 +0100

    button input errors scroller

--- app/screens/CreateHome.js
+++ app/screens/CreateHome.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet , Button , stylesheet , ScrollView} from 'react-native';
 import t from 'tcomb-form-native';
+
+//var ScrollingMenu = require('react-native-scrolling-menu');
+
 
 const Form = t.form.Form;
 
@@ -13,19 +16,76 @@ const Home = t.struct({
     image: t.String
   });
 
+  const options = {
+    fields:{
+      city:{
+      error:'You need a city'
+      },
+      zip:{
+        label:'zip code' ,
+        error: 'You need a zip code'
+      },
+      street:{
+        error:'You need a Street'
+      },
+      description:{
+        error:'You need a description'
+      },
+      location:{
+        error:'You need a Location'
+      },
+      image:{
+        error:'You need an image'
+      }
+  },
+  }
+
+  
+
 export default class CreateHome extends Component {
-    constructor() {
+  handleSubmit = () =>{ const value = this._form.getValue(); // use that ref to get the form value
+    console.log('value: ', value);
+  }  
+  constructor() {
         super()
     }
 
     render() {
         return (
           <View style={styles.container}>
-            <Form type={Home} /> 
-          </View>
+          <ScrollView>
+            <Form 
+              ref ={c => this._form = c}
+              type={Home}
+              options = {options}
+               />
+          <Button
+          title="Submit"
+          onPress={this.handleSubmit}
+        />
+          </ScrollView>
+        </View>
         );
     }
 }    
+
+const formStyles = {
+ ...Form.styleSheet ,
+ controlLabel: {
+  normal: {
+    color: 'blue',
+    fontSize: 18,
+    marginBottom: 7,
+    fontWeight: '600'
+  },
+  error: {
+    color: 'red',
+    fontSize: 18,
+    marginBottom: 7,
+    fontWeight: '600'
+  }
+}
+}
 
 
 const styles = StyleSheet.create({
