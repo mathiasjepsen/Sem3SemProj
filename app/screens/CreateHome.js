1949baff3badfa2226489636bb242c49babea7b6
mathiasjepsen <mathias@wpsnet.com>
2017-12-01 09:01:56 +0100

    .

--- app/screens/CreateHome.js
+++ app/screens/CreateHome.js
@@ -14,61 +14,59 @@ export default class CreateHome extends Component {
             street: '',
             description: '',
             image: ''
-            }
+        }
     }
-    
+
 
     onSubmit = (navigate) => {
         const home = {
-            city : this.state.city,
-            zip : this.state.zip,
-            street : this.state.street,
-            description : this.state.description
+            city: this.state.city,
+            zip: this.state.zip,
+            street: this.state.street,
+            description: this.state.description
         }
         placeFacade.createHome(home)
-        
+
     }
 
-    
-
     render() {
-       
+
         return (
-            <View style={{ 
+            <View style={{
                 flex: 1,
                 flexDirection: 'column',
                 justifyContent: 'center',
                 alignItems: 'center',
-              }}>
-                 <TextInput
-                    style={{ height: 40,width: 320,marginBottom: 24, borderColor: 'gray', borderWidth: 1 }}
+            }}>
+                <TextInput
+                    style={{ height: 40, width: 320, marginBottom: 24, borderColor: 'gray', borderWidth: 1 }}
                     onChangeText={(description) => this.setState({ description })}
                     placeholder='enter description'
                     value={this.state.description}
                 />
                 <TextInput
-                    style={{ height: 40,width: 320, marginBottom: 24,borderColor: 'gray', borderWidth: 1 }}
+                    style={{ height: 40, width: 320, marginBottom: 24, borderColor: 'gray', borderWidth: 1 }}
                     onChangeText={(street) => this.setState({ street })}
                     placeholder='enter street'
                     value={this.state.street}
                 />
                 <TextInput
-                    style={{ height: 40, width: 320, marginBottom: 24,borderColor: 'gray', borderWidth: 1 }}
+                    style={{ height: 40, width: 320, marginBottom: 24, borderColor: 'gray', borderWidth: 1 }}
                     onChangeText={(city) => this.setState({ city })}
                     placeholder='enter city'
                     value={this.state.city}
                 />
                 <TextInput
                     style={{ height: 40, width: 320, marginBottom: 24, borderColor: 'gray', borderWidth: 1 }}
-                    keyboardType = 'numeric'
+                    keyboardType='numeric'
                     onChangeText={(zip) => this.setState({ zip })}
                     placeholder='enter zip'
                     value={this.state.zip}
                 />
                 <Button
-                style={{borderWidth: 10, borderColor: 'blue'}}
-                onPress={this.onSubmit}
-                title="Submit"
+                    style={{ borderWidth: 10, borderColor: 'blue' }}
+                    onPress={this.onSubmit}
+                    title="Submit"
                 //color="#841584"
                 />
             </View>
