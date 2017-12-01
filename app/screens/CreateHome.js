e2b0430df7d8b8350c0436f81c61dba01125ff85
dimitri <cph-dt46@cphbusiness.dk>
2017-12-01 08:56:51 +0100

    margin

--- app/screens/CreateHome.js
+++ app/screens/CreateHome.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { View, StyleSheet, Button, ScrollView, TextInput, Alert } from 'react-native';
 import placeFacade from './../facades/placeFacade';
 
+
+
+
 export default class CreateHome extends Component {
     constructor(props) {
         super(props);
@@ -13,8 +16,9 @@ export default class CreateHome extends Component {
             image: ''
             }
     }
+    
 
-    onSubmit = () => {
+    onSubmit = (navigate) => {
         const home = {
             city : this.state.city,
             zip : this.state.zip,
@@ -22,46 +26,50 @@ export default class CreateHome extends Component {
             description : this.state.description
         }
         placeFacade.createHome(home)
+        
     }
 
+    
+
     render() {
+       
         return (
-            <View>
-                <TextInput
-                    style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-                    onChangeText={(city) => this.setState({ city })}
-                    placeholder='enter city'
-                    value={this.state.city}
-                />
-                <TextInput
-                    style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-                    keyboardType = 'numeric'
-                    onChangeText={(zip) => this.setState({ zip })}
-                    placeholder='enter zip'
-                    value={this.state.zip}
-                />
-                <TextInput
-                    style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-                    onChangeText={(street) => this.setState({ street })}
-                    placeholder='enter street'
-                    value={this.state.street}
-                />
-                <TextInput
-                    style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+            <View style={{ 
+                flex: 1,
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+                 <TextInput
+                    style={{ height: 40,width: 320,marginBottom: 24, borderColor: 'gray', borderWidth: 1 }}
                     onChangeText={(description) => this.setState({ description })}
                     placeholder='enter description'
                     value={this.state.description}
                 />
                 <TextInput
-                    style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-                    onChangeText={(image) => this.setState({ image })}
-                    placeholder='enter image'
-                    value={this.state.image}
+                    style={{ height: 40,width: 320, marginBottom: 24,borderColor: 'gray', borderWidth: 1 }}
+                    onChangeText={(street) => this.setState({ street })}
+                    placeholder='enter street'
+                    value={this.state.street}
+                />
+                <TextInput
+                    style={{ height: 40, width: 320, marginBottom: 24,borderColor: 'gray', borderWidth: 1 }}
+                    onChangeText={(city) => this.setState({ city })}
+                    placeholder='enter city'
+                    value={this.state.city}
+                />
+                <TextInput
+                    style={{ height: 40, width: 320, marginBottom: 24, borderColor: 'gray', borderWidth: 1 }}
+                    keyboardType = 'numeric'
+                    onChangeText={(zip) => this.setState({ zip })}
+                    placeholder='enter zip'
+                    value={this.state.zip}
                 />
                 <Button
+                style={{borderWidth: 10, borderColor: 'blue'}}
                 onPress={this.onSubmit}
                 title="Submit"
-                color="#841584"
+                //color="#841584"
                 />
             </View>
         );
