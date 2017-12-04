183ff1c573d8eab72a37c7faf262a09f9d66c12c
Thomas Thimothee <thom.thimothee@gmail.com>
2017-12-04 09:20:01 +0100

    Role

---
+++ CI-TDD-Spike/src/main/java/Spike/Role.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Spike;
+
+import java.util.List;
+
+/**
+ *
+ * @author thomasthimothee
+ */
+public class Role {
+    
+    private String roleName;
+    private List<User> users;
+
+    public Role(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+    
+    
+    
+}
